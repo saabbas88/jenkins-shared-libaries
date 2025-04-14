3fe521623051f675490abd72cd1ff86f75d50b33
saabbas88 <85310420+saabbas88@users.noreply.github.com>
2025-04-14 14:08:53 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,6 @@
  def call(String Project, String ImageTag, String dockerhubuser){
   wtihCredentials([usernamePassword(credentialsId: 'docker-cred', passwordVariable: 'dockerHubPass',usernameVariables: 'dockerHubUser')])
     sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
-
   }
   sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
-}
+
